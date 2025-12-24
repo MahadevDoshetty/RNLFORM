c476e8ad7776ab753aa7971676052eb634c0d228
MahadevDoshetty <mahadevdoshetty317@gmail.com>
2025-12-24 12:01:04 +0530

    Commit 5

--- server/server.js
+++ server/server.js
@@ -11,7 +11,6 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
-app.options('*', cors());
 app.use(express.json());
 
 app.post("/register", async (req, res) => {
